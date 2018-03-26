84ba2170ffd9153132690d1d7f34af0b9ea15cc8
Jia Yong <williamcjjy@live.com>
2018-03-26 03:21:07 +0000

    back to php bois

--- Personal_Statement.php
+++ Personal_Statement.php
@@ -20,7 +20,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="resume.html">Jia Yong Chong</a>
+      <a class="navbar-brand" href="resume.php">Jia Yong Chong</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
--- resume.php
+++ resume.php
@@ -19,7 +19,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Jia Yong Chong</a>
+      <a class="navbar-brand" href="resume.php">Jia Yong Chong</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
